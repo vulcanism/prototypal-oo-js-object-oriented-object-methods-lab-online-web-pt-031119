ca34252a3874b8d8f03645ea4a0d3e746a2784bc
Savannah C <sgcorrell@gmail.com>
2019-11-02 16:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
 }
+
+BoardMember.prototype.veto = function() {
+  return `No, I must disagree`
+}
